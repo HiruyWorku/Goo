4976b1f5ed99589c0e5d901c0aa817f2613a322a
Hiruy Worku <hworku27@wooster.edu>
2025-02-11 20:06:33 -0500

    first day

---
+++ helloworld.go
@@ -0,0 +1,21 @@
+//what are the main difference and similarities between go an python
+// 1, Go is built for handling concurrency, Python is not
+// 2, Go doesnt have OOP features, specifically Objects, classes and inheritance.
+// 3, Go is a complied while, python is an interpreted language
+// 4, Both python and go have automatic garbage collection
+// 5, Go is statically typed while python is Dynamicly typed 
+
+package main
+import ("fmt")
+
+func main() {
+	fmt.Println("Hello World!")
+}
+
+// in php
+//<?php
+
+//print "Hello, World!";
+
+//?>
+
